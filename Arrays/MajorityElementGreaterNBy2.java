8d52bf555a1bc5c17efd2b1f732d422409a3fae4
Sujoy Peter <sujoypeter0512@gmail.com>
2026-01-06 22:14:47 +0530

    Find the Majority Element that occurs more than N/2 times

---
+++ Arrays/MajorityElementGreaterNBy2.java
@@ -0,0 +1,56 @@
+package Arrays;
+
+public class MajorityElementGreaterNBy2 {
+    public int majorityElement(int[] nums) {
+        // Size of the given array
+        int n = nums.length;
+        
+        // Count variable
+        int cnt = 0;
+        
+        // Candidate element
+        int el = 0;
+        
+        // Step 1: Find the potential majority element
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            } else if (el == nums[i]) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+        
+        // Step 2: Verify the candidate
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) {
+                cnt1++;
+            }
+        }
+        
+        // Return the element if it's a majority
+        if (cnt1 > (n / 2)) {
+            return el;
+        }
+        
+        // No majority found
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        
+        // Create an instance of MajorityElementGreaterNBy2 class
+        MajorityElementGreaterNBy2 sol = new MajorityElementGreaterNBy2();
+
+        // Call the majorityElement function
+        int ans = sol.majorityElement(arr);
+        
+        // Print the majority element
+        System.out.println("The majority element is: " + ans);
+}
+}
