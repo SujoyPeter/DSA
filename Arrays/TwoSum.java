b95548e96739dcbd9d4842da8022b45c98733207
Sujoy Peter <sujoypeter0512@gmail.com>
2026-01-09 18:57:46 +0530

    Two Sum : Check if a pair with given sum exists in Array

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,47 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public int[] twoSumIndices(int[] arr, int target) {
+        int n = arr.length;
+        int[][] numsWithIndex = new int[n][2];
+        
+        // Store element with original index
+        for (int i = 0; i < n; i++) {
+            numsWithIndex[i][0] = arr[i];
+            numsWithIndex[i][1] = i;
+        }
+        
+        // Sort by the value to apply two-pointer
+        Arrays.sort(numsWithIndex, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = numsWithIndex[left][0] + numsWithIndex[right][0];
+            if (sum == target) {
+                // Return original indices of the two numbers found
+                return new int[] {numsWithIndex[left][1], numsWithIndex[right][1]};
+            } else if (sum < target) {
+                // Increase sum by moving left pointer forward
+                left++;
+            } else {
+                // Decrease sum by moving right pointer backward
+                right--;
+            }
+        }
+        
+        // No pair found
+        return new int[] {-1, -1};
+    }
+
+    public static void main(String[] args) {
+        TwoSum sol = new TwoSum();
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+
+        System.out.println(sol.twoSumIndices(arr, target)); // Output: YES
+        int[] res = sol.twoSumIndices(arr, target);
+        System.out.println("[" + res[0] + ", " + res[1] + "]"); // Output: [1, 3]
+    }
+}
