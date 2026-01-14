cd0254fe3558fca77bae4fde7e4bd15c4ea02f5f
Sujoy Peter <sujoypeter0512@gmail.com>
2026-01-14 23:04:04 +0530

    Reverse a Linked list

---
+++ Linked_List/ReverseLinkedList.java
@@ -0,0 +1,58 @@
+package Linked_List;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    // Constructor to initialize node value
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+     public ListNode reverseList(ListNode head) {
+        // Initialize previous pointer to null
+        ListNode prev = null;
+
+        // Start from the head of the list
+        ListNode temp = head;
+
+        // Traverse the list
+        while (temp != null) {
+            // Save the next node
+            ListNode front = temp.next;
+
+            // Reverse the current node's pointer
+            temp.next = prev;
+
+            // Move prev to current node
+            prev = temp;
+
+            // Move to the next node
+            temp = front;
+        }
+
+        // Return new head (last node becomes first)
+        return prev;
+    }
+    public static void main(String[] args) {
+        // Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ReverseLinkedList obj = new ReverseLinkedList();
+        ListNode reversedHead = obj.reverseList(head);
+
+        // Print the reversed linked list
+        ListNode current = reversedHead;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+}
