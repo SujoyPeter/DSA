cd6b1612bfe21062e2d324676cdab1f24d8be4bb
Sujoy Peter <sujoypeter0512@gmail.com>
2026-01-16 23:21:19 +0530

    Merge two sorted Linked Lists

---
+++ Linked_List/MergeTwoSortedList.java
@@ -0,0 +1,65 @@
+package Linked_List;
+  class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+public class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+         // as the head of the merged list
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+
+        // Traverse both lists simultaneously
+        while (list1 != null && list2 != null) {
+            // Compare elements of both lists and
+            // link the smaller node to the merged list
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            // Move the temporary pointer
+            // to the next node
+            temp = temp.next;
+        }
+
+        // If any list still has remaining
+        // elements, append them to the merged list
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+        // Return the merged list starting
+        // from the next of the dummy node
+        return dummyNode.next;
+    }
+    public static void main(String[] args) {
+        MergeTwoSortedList sol = new MergeTwoSortedList();
+
+        // Creating first sorted linked list: 1 -> 3 -> 5
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+
+        // Creating second sorted linked list: 2 -> 4 -> 6
+        ListNode list2 = new ListNode(2);
+        list2.next = new ListNode(4);
+        list2.next.next = new ListNode(6);
+
+        // Merging the two sorted linked lists
+        ListNode mergedList = sol.mergeTwoLists(list1, list2);
+
+        // Printing the merged linked list
+        System.out.println("Merged Sorted Linked List:");
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+}
