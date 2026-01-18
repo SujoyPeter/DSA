a610ddedce0ec8f035eb1e8e4131987eb125929f
Sujoy Peter <sujoypeter0512@gmail.com>
2026-01-19 00:07:13 +0530

    Delete given node in a Linked List : O(1) approach

---
+++ Linked_List/DeleteNodeInAList.java
@@ -0,0 +1,37 @@
+package Linked_List;
+ class ListNode {
+     int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+public class DeleteNodeInAList {
+    public void deleteNode(ListNode node) {
+         // Copy value of next node into current node
+        node.val = node.next.val;
+
+        // Bypass the next node
+        node.next = node.next.next;
+    }
+    public static void main(String[] args) {
+        DeleteNodeInAList solution = new DeleteNodeInAList();
+
+        // Example: Creating a linked list 4 -> 5 -> 1 -> 9
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+
+        // Node to be deleted (node with value 5)
+        ListNode nodeToDelete = head.next;
+
+        // Delete the node
+        solution.deleteNode(nodeToDelete);
+
+        // Print the updated linked list
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+}
