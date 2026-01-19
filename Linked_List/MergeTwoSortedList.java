8f71d7ebf5254619d90f7740a2d446fc0124fcc9
Sujoy Peter <sujoypeter0512@gmail.com>
2026-01-20 00:36:25 +0530

    Merge two sorted Linked Lists

--- Linked_List/MergeTwoSortedList.java
+++ Linked_List/MergeTwoSortedList.java
@@ -1,11 +1,4 @@
 package Linked_List;
-  class ListNode {
-     int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
 public class MergeTwoSortedList {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
          // as the head of the merged list
