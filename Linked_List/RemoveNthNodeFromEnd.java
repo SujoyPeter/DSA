7c500380b6a1a4b3505bdf2f872bde5334a20f31
Sujoy Peter <sujoypeter0512@gmail.com>
2026-01-20 00:38:03 +0530

    Remove N-th node from the end of a Linked List

--- Linked_List/RemoveNthNodeFromEnd.java
+++ Linked_List/RemoveNthNodeFromEnd.java
@@ -1,11 +1,5 @@
 package Linked_List;
-  class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+ 
 public class RemoveNthNodeFromEnd {
      public ListNode removeNthFromEnd(ListNode head, int n) {
         // Create a dummy node before head to handle edge cases
